40248af621dc2be5820e7622a8b2d1906012a0e0
Toivancenobi <tobias.ettling@gmx.de>
2023-08-16 12:30:51 +0200

    edited slurm_test.sh

--- slurm_test.py
+++ slurm_test.py
@@ -39,98 +39,98 @@ if device.type == 'cuda':
 else:
     print('Using CPU :(')
 
-fb = ['delta', 'theta', 'alpha', 'beta', 'whole_spec']
-md = ['delta_quantile', 'theta_quantile', 'alpha_quantile', 'beta_quantile', 'whole_spec_quantile']
-
-dl = DatasetLoader(40000, ['EC', 'EO'], fb)
-dl.check_cache()
-
-x_data = dl.x_data
-y_data = dl.y_data
-group = dl.group
-sample_ids = dl.sample_ids
-
-gkf = GroupKFold(n_splits=5)
-train_index, test_index = next(iter(gkf.split(x_data, y_data, group)))
-
-x_train, x_test = [x_data[i] for i in train_index], [x_data[i] for i in test_index]
-y_train, y_test = [y_data[i] for i in train_index], [y_data[i] for i in test_index]
-
-train_data = Data(x_train, y_train, device)
-test_data = Data(x_test, y_test, device)
-
-# Create a file to save our model scores with their parameters.
-dir_path = r"/home/modelrep/sadiya/tobias_ettling/data/results/"
-res_path = dir_path + f'hpt_{pid}.csv'
-
-# Check to avoid overwrite
-if not (os.path.isfile(res_path)):
-    # Create dataframe with parameters/score columns
-    df = pd.DataFrame(
-        columns=['MAE score P1', 'MAE score P2', 'loss', 'learning_rate', 'batch_size', 'hidden_sizes', 'epochs',
-                 'activation', 'optimizer', 'early stopping'])
-    df.to_csv(res_path)
-
-hp_space = load_object('/home/modelrep/sadiya/tobias_ettling/HPC_FIN/hpt_space')
-
-for i in range(0, 2):
-    if i % num_nodes == pid:
-        hyper_param = hp_space[i]
-        hyper_param['device'] = device
-        hyper_param['input_size'] = len(fb * 100)
-
-        ml = ModelLoader(md)  # , 'whole_spec_quantile'
-        ml.build_fin_models()
-        stage_1 = ml.fin_models
-        stage_2 = DynamicNeuralNetwork(
-            input_size=hyper_param['input_size'],
-            output_size=hyper_param['output_size'],
-            hidden_sizes=hyper_param['hidden_sizes'],
-            activ=hyper_param['activation'],
-            dropout_p=hyper_param['dropout_p']
-        )
-
-        model = FeatureImitationNetwork(stage_1, stage_2, dl.data_order, device, freeze_fins=True)
-        model.to(device)
-
-        early_stopping = EarlyStopping(tolerance=3, min_delta=0.2)
-
-        fit_param = dict(
-            learning_rate=hyper_param['learning_rate'],
-            batch_size=hyper_param['batch_size'],
-            epochs=hyper_param['epochs'],
-            weight_decay=hyper_param['weight_decay'],
-            momentum=hyper_param['momentum'],
-            activation=hyper_param['activation'],
-            sched_ss=hyper_param['sched_ss'],
-            sched_g=hyper_param['sched_g']
-        )
-        eval_score = []
-        for phase in [1, 2]:
-            if phase == 2:
-                model.unfreeze_fins()
-
-            fin_trainer = FINTrainer(
-                model,
-                fit_param,
-                train_data,
-                test_data,
-                early_stop=early_stopping,
-            )
-
-            score = fin_trainer.fit_model()
-
-            print(f'Phase {phase} - Evaluation MAE: ', score)
-            eval_score.append(score)
-
-        # Read in the results
-        r = pd.read_csv(res_path, index_col=0)
-
-        # Add new results
-        r.loc[len(r.index)] = [eval_score[0], eval_score[1], min(fin_trainer.loss_log['train_loss']),
-                               fit_param['learning_rate'], fit_param['batch_size'], str(fit_param['hidden_sizes']),
-                               fit_param['epochs'],
-                               fit_param['activation'], fit_param['optimizer'],
-                               fin_trainer.early_stopping.early_stop]
-        print(r.loc[len(r.index) - 1])
-        r.to_csv(res_path)
+# fb = ['delta', 'theta', 'alpha', 'beta', 'whole_spec']
+# md = ['delta_quantile', 'theta_quantile', 'alpha_quantile', 'beta_quantile', 'whole_spec_quantile']
+#
+# dl = DatasetLoader(40000, ['EC', 'EO'], fb)
+# dl.check_cache()
+#
+# x_data = dl.x_data
+# y_data = dl.y_data
+# group = dl.group
+# sample_ids = dl.sample_ids
+#
+# gkf = GroupKFold(n_splits=5)
+# train_index, test_index = next(iter(gkf.split(x_data, y_data, group)))
+#
+# x_train, x_test = [x_data[i] for i in train_index], [x_data[i] for i in test_index]
+# y_train, y_test = [y_data[i] for i in train_index], [y_data[i] for i in test_index]
+#
+# train_data = Data(x_train, y_train, device)
+# test_data = Data(x_test, y_test, device)
+#
+# # Create a file to save our model scores with their parameters.
+# dir_path = r"/home/modelrep/sadiya/tobias_ettling/data/results/"
+# res_path = dir_path + f'hpt_{pid}.csv'
+#
+# # Check to avoid overwrite
+# if not (os.path.isfile(res_path)):
+#     # Create dataframe with parameters/score columns
+#     df = pd.DataFrame(
+#         columns=['MAE score P1', 'MAE score P2', 'loss', 'learning_rate', 'batch_size', 'hidden_sizes', 'epochs',
+#                  'activation', 'optimizer', 'early stopping'])
+#     df.to_csv(res_path)
+#
+# hp_space = load_object('/home/modelrep/sadiya/tobias_ettling/HPC_FIN/hpt_space')
+#
+# for i in range(0, 2):
+#     if i % num_nodes == pid:
+#         hyper_param = hp_space[i]
+#         hyper_param['device'] = device
+#         hyper_param['input_size'] = len(fb * 100)
+#
+#         ml = ModelLoader(md)  # , 'whole_spec_quantile'
+#         ml.build_fin_models()
+#         stage_1 = ml.fin_models
+#         stage_2 = DynamicNeuralNetwork(
+#             input_size=hyper_param['input_size'],
+#             output_size=hyper_param['output_size'],
+#             hidden_sizes=hyper_param['hidden_sizes'],
+#             activ=hyper_param['activation'],
+#             dropout_p=hyper_param['dropout_p']
+#         )
+#
+#         model = FeatureImitationNetwork(stage_1, stage_2, dl.data_order, device, freeze_fins=True)
+#         model.to(device)
+#
+#         early_stopping = EarlyStopping(tolerance=3, min_delta=0.2)
+#
+#         fit_param = dict(
+#             learning_rate=hyper_param['learning_rate'],
+#             batch_size=hyper_param['batch_size'],
+#             epochs=hyper_param['epochs'],
+#             weight_decay=hyper_param['weight_decay'],
+#             momentum=hyper_param['momentum'],
+#             activation=hyper_param['activation'],
+#             sched_ss=hyper_param['sched_ss'],
+#             sched_g=hyper_param['sched_g']
+#         )
+#         eval_score = []
+#         for phase in [1, 2]:
+#             if phase == 2:
+#                 model.unfreeze_fins()
+#
+#             fin_trainer = FINTrainer(
+#                 model,
+#                 fit_param,
+#                 train_data,
+#                 test_data,
+#                 early_stop=early_stopping,
+#             )
+#
+#             score = fin_trainer.fit_model()
+#
+#             print(f'Phase {phase} - Evaluation MAE: ', score)
+#             eval_score.append(score)
+#
+#         # Read in the results
+#         r = pd.read_csv(res_path, index_col=0)
+#
+#         # Add new results
+#         r.loc[len(r.index)] = [eval_score[0], eval_score[1], min(fin_trainer.loss_log['train_loss']),
+#                                fit_param['learning_rate'], fit_param['batch_size'], str(fit_param['hidden_sizes']),
+#                                fit_param['epochs'],
+#                                fit_param['activation'], fit_param['optimizer'],
+#                                fin_trainer.early_stopping.early_stop]
+#         print(r.loc[len(r.index) - 1])
+#         r.to_csv(res_path)
