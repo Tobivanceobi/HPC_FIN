8dda65245f1287bfa2ba9b6e80d44720bd5161c5
Toivancenobi <tobias.ettling@gmx.de>
2023-08-16 13:25:06 +0200

    edited slurm_test.sh

--- slurm_start.sh
+++ slurm_start.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=10
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4000
-#SBATCH --time=00:10:00
-#SBATCH --array=0-2
+#SBATCH --mem-per-cpu=8000
+#SBATCH --time=05:00:00
+#SBATCH --array=0-9
 #SBATCH --mail-type=FAIL
 #SBATCH --output out/output_%a.txt
 #SBATCH --error err/error_%a.txt
@@ -13,6 +13,10 @@
 # Remove previous results
 #rm err/*; rm out/*; rm -r runs/*;
 
-source $HOME/tobias_ettling/HPC_FIN/venv/bin/activate
+source $HOME/miniconda/etc/profile.d/conda.sh
+conda init bash
+conda activate pytorch
+
 python3 $HOME/tobias_ettling/HPC_FIN/slurm_start.py $SLURM_ARRAY_TASK_ID $SLURM_NNODES
-deactivate
+
+conda deactivate
