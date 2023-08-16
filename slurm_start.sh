32aefd27c28a72b3c14bbca0478c8ec897d9442b
Toivancenobi <tobias.ettling@gmx.de>
2023-08-16 16:10:00 +0200

    changed mem and cpu in start script

--- slurm_start.sh
+++ slurm_start.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10000
-#SBATCH --tmp=10000
+#SBATCH --mem-per-cpu=5000
+#SBATCH --tmp=100000
 #SBATCH --time=05:00:00
 #SBATCH --array=0-8
 #SBATCH --mail-type=FAIL
