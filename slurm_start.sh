c016ee33ae6948b1361dd0e706e1c7456e3b16c9
Toivancenobi <tobias.ettling@gmx.de>
2023-08-16 17:05:20 +0200

    changed mem and cpu in start script

--- slurm_start.sh
+++ slurm_start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=FIN-Brainage
-#SBATCH --partition=gpu43
+#SBATCH --partition=gpu
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=10000
 #SBATCH --time=05:00:00
 #SBATCH --array=0-8
 #SBATCH --mail-type=FAIL
