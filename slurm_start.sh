59109228d4a0ca0d4621c73c4fd846d7233338b9
Toivancenobi <tobias.ettling@gmx.de>
2023-08-16 14:34:31 +0200

    added logging

--- slurm_start.sh
+++ slurm_start.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=10
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=8000
+#SBATCH --mem-per-cpu=5000
 #SBATCH --time=05:00:00
 #SBATCH --array=0-9
 #SBATCH --mail-type=FAIL
