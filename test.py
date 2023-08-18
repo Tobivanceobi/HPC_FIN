8f53556c23bfd2a9d3573c96e6091604970d0fc6
Toivancenobi <tobias.ettling@gmx.de>
2023-08-18 19:10:16 +0200

    changed if used con in start

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import torch
-from sklearn.model_selection import GroupKFold
+from sklearn.model_selection import GroupKFold, StratifiedGroupKFold, StratifiedKFold
 
 from src.datasetLoader import DatasetLoader
 from src.finTrainer import FINTrainer
@@ -92,95 +92,12 @@ dl.get_y_data()
 
 x_data = dl.x_data
 y_data = dl.y_data
-
 group = dl.group
-sample_ids = dl.sample_ids
-
-gkf = GroupKFold(n_splits=5)
-train_index, test_index = next(iter(gkf.split(x_data, y_data, group)))
-
+print(y_data)
+y_stf = [int(age*10) for age in y_data]
+equalize_classes(y_data)
+skf_vals = []
+skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=126)
+train_index, test_index = next(iter(skf.split(x_data, y_stf, group)))
 x_train, x_test = [x_data[i] for i in train_index], [x_data[i] for i in test_index]
 y_train, y_test = [y_data[i] for i in train_index], [y_data[i] for i in test_index]
-
-train_data = Data(x_train, y_train, device)
-test_data = Data(x_test, y_test, device)
-
-# Create a file to save our model scores with their parameters.
-dir_path = r"/home/modelrep/sadiya/tobias_ettling/HPC_FIN/results/"
-res_path = dir_path + f'hpt_{pid}.csv'
-
-# Check to avoid overwrite
-if not (os.path.isfile(res_path)):
-    # Create dataframe with parameters/score columns
-    df = pd.DataFrame(
-        columns=['MAE score P1', 'MAE score P2', 'loss', 'learning_rate', 'batch_size', 'hidden_sizes', 'epochs',
-                 'activation', 'optimizer', 'early stopping'])
-    df.to_csv(res_path)
-
-hp_space = load_object(f'./hp_params/hptSpace_{pid}')
-for i in range(0, len(hp_space)):
-    hyper_param = hp_space[i]
-
-    if check_if_param_used(hyper_param, res_path):
-        print('Already used Hyperparameters: ', hyper_param)
-        continue
-
-    hyper_param['device'] = device
-    hyper_param['input_size'] = len(fb * 100)
-
-    ml = ModelLoader(md)  # , 'whole_spec_quantile'
-    ml.build_fin_models()
-    stage_1 = ml.fin_models
-    stage_2 = DynamicNeuralNetwork(
-        input_size=hyper_param['input_size'],
-        output_size=hyper_param['output_size'],
-        hidden_sizes=hyper_param['hidden_sizes'],
-        activ=hyper_param['activation'],
-        dropout_p=hyper_param['dropout_p']
-    )
-
-    model = FeatureImitationNetwork(stage_1, stage_2, dl.data_order, device, freeze_fins=True)
-    model.to(device)
-
-    early_stopping = EarlyStopping(tolerance=5, min_delta=1)
-
-    fit_param = dict(
-        learning_rate=hyper_param['learning_rate'],
-        batch_size=hyper_param['batch_size'],
-        epochs=hyper_param['epochs'],
-        weight_decay=hyper_param['weight_decay'],
-        momentum=hyper_param['momentum'],
-        activation=hyper_param['activation'],
-        sched_ss=hyper_param['sched_ss'],
-        sched_g=hyper_param['sched_g']
-    )
-    eval_score = []
-    for phase in [1, 2]:
-        if phase == 2:
-            model.unfreeze_fins()
-
-        fin_trainer = FINTrainer(
-            model,
-            fit_param,
-            train_data,
-            test_data,
-            early_stop=early_stopping,
-        )
-
-        score = fin_trainer.fit_model()
-
-        print(f'Phase {phase} - Evaluation MAE: ', score)
-        eval_score.append(score)
-
-    # Read in the results
-    r = pd.read_csv(res_path, index_col=0)
-
-    # Add new results
-    r.loc[len(r.index)] = [eval_score[0], eval_score[1], min(fin_trainer.loss_log['train_loss']),
-                           hyper_param['learning_rate'], hyper_param['batch_size'],
-                           str(hyper_param['hidden_sizes']),
-                           hyper_param['epochs'],
-                           hyper_param['activation'], hyper_param['optimizer'],
-                           fin_trainer.early_stopping.early_stop]
-    print(r.loc[len(r.index) - 1])
-    r.to_csv(res_path)
