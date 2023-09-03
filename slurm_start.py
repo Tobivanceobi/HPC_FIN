b471b5ef20b1b0be26a3ce603344c883a3356c0d
Toivancenobi <tobias.ettling@gmx.de>
2023-09-03 15:06:31 +0200

    added state selection do dataloader

--- slurm_start.py
+++ slurm_start.py
@@ -16,39 +16,6 @@ from src.network.finNN import FeatureImitationNetwork
 from src.network.utiles import Data, EarlyStopping
 
 
-def equalize_classes(targets, threshold=3):
-    l = np.unique(np.array(targets), return_counts=True)
-    tar_map = []
-    for i in range(len(l[0])):
-        if l[1][i] < threshold:
-            c_up = i
-            c_down = i
-            con = True
-            con2 = False
-            while con:
-                if c_up + 1 < len(l[0]):
-                    c_up += 1
-                    if l[1][c_up] >= threshold:
-                        tar_map.append([l[0][i], l[0][c_up]])
-                        con = False
-
-                if c_down - 1 >= 0 and con:
-                    c_down -= 1
-                    if l[1][c_down] >= threshold:
-                        tar_map.append([l[0][i], l[0][c_down]])
-                        con = False
-
-    tar_map_t = np.array(tar_map).transpose()
-    y_new = []
-    for age in targets:
-        if age in tar_map_t[0]:
-            id_age = list(tar_map_t[0]).index(age)
-            y_new.append(tar_map_t[1][id_age])
-        else:
-            y_new.append(age)
-    return y_new
-
-
 def check_if_param_used(param, hpt_path):
     hpt_df = pd.read_csv(hpt_path, index_col=0)
     condition = (
